ff115ff5527e48373d623bb36df18f137f14a976
anton <milosoiu.anton@gmail.com>
2020-09-06 10:05:36 +0300

    Made some changes to premake.

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
   
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,7 +21,6 @@ project "GLFW"
   
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
     
 		files
 		{
@@ -42,12 +42,12 @@ project "GLFW"
 		}
 
 	filter "configurations:Debug"
-	runtime "Debug"
-	symbols "On"
+		runtime "Debug"
+		symbols "on"
     
 	filter "configurations:Release"
-	runtime "Release"
-	optimize "On"
+		runtime "Release"
+		optimize "on"
       
       
       
